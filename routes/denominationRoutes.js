91a2e4625f82f14593d8ee1b6769bf3372ccc7f4
kevinpaezg <kevin.paez1010@gmail.com>
2022-04-08 11:57:30 -0500

    CRUD denominations completed

--- routes/denominationRoutes.js
+++ routes/denominationRoutes.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const { index, create, show, update, destroy } = require('../controllers/denominationController')
+//Function validate fields
+const { validate } = require('../middleware/validators/denominationValidator')
 
-//aqui poner el codigo
+//ruta version 1
+const { PATH_V1 } = require('./1-paths')
 
-module.exports = router
+//denomination
+router.get(`${PATH_V1}/denominations/`, index);
+router.post(`${PATH_V1}/denominations/`, validate, create);
+router.get(`${PATH_V1}/denominations/:id`, show)
+router.put(`${PATH_V1}/denominations/:id`, validate, update)
+router.delete(`${PATH_V1}/denominations/:id`, destroy)
+
+module.exports = router;
