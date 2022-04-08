357d09ee5c9df4bb797df8aa7a348c7b87def4d4
Jose Guadalupe Gaitan Marquez <gaitan138@gmail.com>
2022-04-08 12:36:37 -0500

    [IMP] getclavetoken

--- routes/claveRoutes.js
+++ routes/claveRoutes.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const router = express.Router()
+//funciones del controlador
+const {create,validateT} = require('../controllers/tokenController')
+//funcion para validar campos
+const {validate} = require('../middleware/validators/tokenValidator')
+//ruta version 1
+const {PATH_V1} = require('./1-paths')
 
-//aqui poner el codigo
+//anomalies
+router.put(`${PATH_V1}/token/:id`,create)
+router.post(`${PATH_V1}/token/:id`,validate,validateT)
+
 
 module.exports = router
