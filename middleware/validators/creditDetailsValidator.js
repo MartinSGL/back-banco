a4b96bb9dba62235c79785935a99f48062c22282
IvanYboa <yboa80@gmail.com>
2022-04-24 12:44:03 -0500

    [IMP] interest and creditdetails

---
+++ middleware/validators/creditDetailsValidator.js
@@ -0,0 +1,16 @@
+const {check} = require('express-validator');
+const {validator} = require('../../helpers/validator');
+
+const validate = [
+
+    check('name').not().isEmpty(),
+    check('interest').not().isEmpty().isFloat(),
+    check('extra_charge').not().isEmpty().isFloat(),
+
+    (req,res,next)=>{
+        validator(req,res,next)
+    }
+
+]
+
+module.exports = {validate} 
--- routes/interestRoutes.js
+++ routes/interestRoutes.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const {create,index,update,destroy} = require('../controllers/interestsController');
+const {validate} = require('../middleware/validators/interestsValidator');
+const validateToken = require('../middleware/validateToken');
+const {PATH_V1} = require('./1-paths');
+
 
 //codigo
+router.get(`${PATH_V1}/interests`,validateToken,index);
+router.post(`${PATH_V1}/interests`,[validateToken,validate],create);
+router.put(`${PATH_V1}/interests/:id`,[validateToken,validate],update);
+router.delete(`${PATH_V1}/interests/:id`,validateToken,destroy);
+
 
 
 module.exports = router
