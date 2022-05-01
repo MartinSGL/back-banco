b668f121d542d704979f8428a53f8a1bfac61ad4
Chavin30 <martingaytan.lugo@gmail.com>
2022-05-01 12:34:13 -0500

    [IMP] index page and documentation for API

--- index.js
+++ index.js
@@ -6,13 +6,19 @@ app.use(cors())
 //usar las variables de entorno
 require('dotenv').config();
 
+const path = require('path');
+
 app.use(express.json())
 app.use(express.urlencoded(({extended:true})))
 
 const PORT = process.env.PORT || 9000;
 
-app.get('/', (req,res)=>{
-    res.send('welcome to API Bancomex')
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+
+app.get('/documentation', (req,res)=>{
+    res.redirect('https://sky-session-d09.notion.site/Bantexico-Project-68933055a7b3436dbb41502ce1b4f210')
 })
 
 //usar las rutas
